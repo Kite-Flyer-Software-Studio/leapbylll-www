4744ee6e3b48f69b2b96d219d3e7266f33b691e0
reico <leereico@gmail.com>
2026-02-12 12:00:21 +0700

    refactor: update Hero component layout and animations, modify English translations for improved clarity and consistency

--- src/components/pages/home/hero.tsx
+++ src/components/pages/home/hero.tsx
@@ -38,7 +38,7 @@ export const Hero = () => {
           ease: "easeOut",
           duration: 0.5,
         }}
-        className="text-3xl md:text-4xl lg:text-8xl font-semibold max-w-6xl mx-auto text-center relative z-10"
+        className="text-3xl md:text-5xl lg:text-8xl font-semibold max-w-6xl mx-auto text-center relative z-10 flex flex-col"
       >
         <Balancer>
           <RoughNotationGroup show={isTitleInView}>
@@ -52,19 +52,18 @@ export const Hero = () => {
             >
               <span className="text-currentColor">{t('title.accountant')}</span>
             </RoughNotation>{" "}
-            {t('title.part2')}{" "}
-            <RoughNotation
+            {/* <RoughNotation
               type="underline"
               animationDuration={2000}
               iterations={10}
               color="#22c55e"
-            >
-              {t('title.startToGrowth')}
-            </RoughNotation>.
+            > */}
+            {/* </RoughNotation>. */}
           </RoughNotationGroup>
         </Balancer>
+        <span>{` ${t('title.startToGrowth')}`}</span>
       </motion.h1>
-      <motion.p
+      <motion.div
         initial={{
           y: 40,
           opacity: 0,
@@ -76,95 +75,77 @@ export const Hero = () => {
         transition={{
           ease: "easeOut",
           duration: 0.5,
-          delay: 0.1,
+          delay: 0.2,
         }}
-        className="text-center mt-12 text-lg md:text-2xl font-medium text-neutral-700 dark:text-neutral-300 max-w-4xl mx-auto relative z-10"
+        className="text-center mt-6 text-base md:text-xl text-muted dark:text-muted-dark max-w-3xl mx-auto relative z-10"
       >
         <Balancer>
-          {t('badge')}
-        </Balancer>
-      </motion.p>
-        <motion.div
-          initial={{
-            y: 40,
-            opacity: 0,
-          }}
-          animate={{
-            y: 0,
-            opacity: 1,
-          }}
-          transition={{
-            ease: "easeOut",
-            duration: 0.5,
-            delay: 0.2,
-          }}
-          className="text-center mt-6 text-base md:text-xl text-muted dark:text-muted-dark max-w-3xl mx-auto relative z-10"
-        >
-          <Balancer>
+          <RoughNotationGroup show={isTitleInView}>
             {t('subtitle.prefix')}{" "}
-            <FlipWords
-              words={t.raw('subtitle.businessTypes')}
-              duration={3000}
-              className="font-semibold text-neutral-900 dark:text-neutral-100"
-            />{" "}
-            {t('subtitle.suffix1')}
-            <br />
-            {t('subtitle.suffix2')}
-          </Balancer>
-        </motion.div>
-        <motion.div
-          initial={{
-            y: 80,
-            opacity: 0,
-          }}
-          animate={{
-            y: 0,
-            opacity: 1,
-          }}
-          transition={{
-            ease: "easeOut",
-            duration: 0.5,
-            delay: 0.4,
-          }}
-          className="flex items-center gap-4 justify-center mt-6 relative z-10 px-4"
-        >
-          <Button as={Link} href="/get-quote" className="text-center">{t('getStarted')}</Button>
-          <Button
-            variant="simple"
-            as={Link}
-            href="#services-pricing"
-            className="flex space-x-2 items-center group text-center"
-          >
-            <span>{t('contactUs')}</span>
-            <HiArrowRight className="text-muted group-hover:translate-x-1 stroke-[1px] h-3 w-3 transition-transform duration-200 dark:text-muted-dark" />
-          </Button>
-        </motion.div>
-        <motion.div initial={{
+            <RoughNotation
+              type="underline"
+              animationDuration={2000}
+              iterations={10}
+              color="#22c55e"
+            >
+              <span className="text-currentColor">{t('subtitle.suffix')}</span>
+            </RoughNotation>
+          </RoughNotationGroup>
+        </Balancer>
+      </motion.div>
+      <motion.div
+        initial={{
           y: 80,
           opacity: 0,
         }}
-          animate={{
-            y: 0,
-            opacity: 1,
-          }}
-          transition={{
-            ease: "easeOut",
-            duration: 0.5,
-            delay: 0.6,
-          }} className="mt-10 relative z-10">
-          <p
-            className={twMerge(
-              "text-neutral-400 text-sm text-center relative z-40",
-              "text-center"
-            )}
-          >
-            {t('backedBy')}
-          </p>
-        </motion.div>
-        <div className="mt-20 relative">
-          <HeroIllustration />
-          <div className="absolute inset-x-0 bottom-0 h-40 w-full bg-gradient-to-b from-transparent via-white to-white dark:via-black/50 dark:to-black scale-[1.1] pointer-events-none" />
-        </div>
+        animate={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          ease: "easeOut",
+          duration: 0.5,
+          delay: 0.4,
+        }}
+        className="flex items-center gap-4 justify-center mt-6 relative z-10 px-4"
+      >
+        <Button as={Link} href="/get-quote" className="text-center">{t('getStarted')}</Button>
+        <Button
+          variant="simple"
+          as={Link}
+          href="#services-pricing"
+          className="flex space-x-2 items-center group text-center"
+        >
+          <span>{t('contactUs')}</span>
+          <HiArrowRight className="text-muted group-hover:translate-x-1 stroke-[1px] h-3 w-3 transition-transform duration-200 dark:text-muted-dark" />
+        </Button>
+      </motion.div>
+      <motion.div initial={{
+        y: 80,
+        opacity: 0,
+      }}
+        animate={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          ease: "easeOut",
+          duration: 0.5,
+          delay: 0.6,
+        }} className="mt-10 relative z-10">
+        <p
+          className={twMerge(
+            "text-neutral-400 text-sm text-center relative z-40",
+            "text-center"
+          )}
+        >
+          {t('backedBy')}
+        </p>
+      </motion.div>
+      <div className="mt-20 relative">
+        <HeroIllustration />
+        <div className="absolute inset-x-0 bottom-0 h-40 w-full bg-gradient-to-b from-transparent via-white to-white dark:via-black/50 dark:to-black scale-[1.1] pointer-events-none" />
+      </div>
     </div >
-  ); 
+  );
 };

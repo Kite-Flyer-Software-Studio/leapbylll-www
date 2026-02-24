efe6dd0d1c29c535eb1ddf05569f55c49109e71c
reico <leereico@gmail.com>
2026-02-25 00:45:09 +0800

    refactor: adjust padding in AboutTeam section for improved layout and visual consistency

--- src/components/pages/about/about-team.tsx
+++ src/components/pages/about/about-team.tsx
@@ -38,7 +38,7 @@ export const AboutTeam = () => {
   return (
     <div
       id="why-leap-by-lll"
-      className="relative z-20 py-10 lg:py-20 overflow-hidden flex flex-col items-center"
+      className="relative z-20 py-20 lg:py-28 overflow-hidden flex flex-col items-center"
     >
       <Badge>{tIntro("badge")}</Badge>
       <Heading className="mt-4" as="h2">
